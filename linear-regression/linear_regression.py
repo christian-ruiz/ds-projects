492ab1f1ea2103cdac174f60a5e2a8e750fd2851
christian-ruiz <62626375+christian-ruiz@users.noreply.github.com>
2023-07-21 19:46:19 -0500

    change settings and read csv

--- linear-regression/linear_regression.py
+++ linear-regression/linear_regression.py
@@ -2,10 +2,16 @@
 # upload packages
 import pandas as pd
 import numpy as np
+import os
+import requests
+import urllib2
+
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
 # import df
-df = pd.read_csv()
+url = 'https://raw.githubusercontent.com/christian-ruiz/ds-projects/main/linear-regression/USA_Housing.csv'
+df = pd.read_csv(url)

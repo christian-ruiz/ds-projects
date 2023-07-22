b790d59ebedf249f8317e319458a4111d5bf0287
christian-ruiz <christianruiz95@gmail.com>
2023-07-22 02:14:41 -0500

    completed git changes

--- linear-regression/linear_regression.py
+++ linear-regression/linear_regression.py
@@ -4,7 +4,6 @@ import pandas as pd
 import numpy as np
 import os
 import requests
-import urllib2
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -13,5 +12,4 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
 # import df
-url = 'https://raw.githubusercontent.com/christian-ruiz/ds-projects/main/linear-regression/USA_Housing.csv'
-df = pd.read_csv(url)
+df = pd.read_csv('linear-regression/USA_Housing.csv')
